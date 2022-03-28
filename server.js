15dd62babc7b7b24108844f0f29ea77646b9f240
Ben Fok <benfok8@gmail.com>
2022-03-28 15:53:40 -0700

    adding ids to seed data and moving session secret to server.js file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 
 // Configure and link a session object with the sequelize store
 const sess = {
-  secret: process.env.SESS_SECRET,
+  secret: 'Secret secret secret',
   cookie: {
     // Session expires after 1 min of inactivity.
     expires: 60000
