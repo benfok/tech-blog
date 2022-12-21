adf7327b85d344d50cf4457264137f3bee5bdb2b
Ben Fok <benfok8@gmail.com>
2022-12-21 10:44:27 -0800

    test4

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-//test comment
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-if (process.env.CYCLIC_DB) {
-  console.log(process.env.CYCLIC_DB);
-  sequelize = new Sequelize(process.env.CYCLIC_DB);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
